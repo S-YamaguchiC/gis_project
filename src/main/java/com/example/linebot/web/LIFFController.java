7a7c5344d6470057df274e3255c9c0a0a8b99e4c
S-YamaguchiC <cist.b216.s.yamaguchi@gmail.com>
2018-10-24 17:58:21 +0900

    画像リサイズ用クラスを別で作成

--- src/main/java/com/example/linebot/web/LIFFController.java
+++ src/main/java/com/example/linebot/web/LIFFController.java
@@ -10,19 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
-import java.awt.image.BufferedImage;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.sql.SQLException;
-import java.util.Base64;
-import java.util.Optional;
-
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 
 @Controller
@@ -43,12 +32,6 @@ public class LIFFController {
         return "liff";
     }
 
-
-    /*
-     *  次の画面で入力した報告の表示
-     *  ※ここでサーバーに送信するなりDBに保存する
-     *
-     */
 
     /*
     * @param type 報告の種別
@@ -81,6 +64,7 @@ public class LIFFController {
         modelMap.addAttribute("lat", lat);
         modelMap.addAttribute("lng", lng);
 
+        //画像の有無
         boolean exist = false;
 
         //nullチェック
@@ -89,9 +73,12 @@ public class LIFFController {
             return "/result";
         }
 
+        //リサイズのためのインスタンス
+        ResizeImage resizeImage = new ResizeImage();
+
         //base64にした画像データ
         //String base64encodingStr = base64EncodeStr(mfile);
-        String base64encodingStr = base64EncodeStrV2(mfile);
+        String base64encodingStr = resizeImage.base64EncodeStrV2(mfile);
         modelMap.addAttribute("base64","data:image/png;base64," + base64encodingStr);
 
         //画像がNULLじゃないはずなので、trueにして送信
@@ -101,134 +88,8 @@ public class LIFFController {
         return "/result";
     }
 
-    // MultipartFileのいんぷっとストリームを、拡張子を指定してファイルに書き込む
-    // また保存先のファイルパスをOptional型で返す
-    private Optional<String> makeTmpFile(MultipartFile multipartFile, String extention) {
 
-        try(InputStream is = multipartFile.getInputStream()){
-            Path tmpFilePath = Files.createTempFile("liff", extention);
-            Files.copy(is, tmpFilePath,REPLACE_EXISTING);
-            return Optional.ofNullable(tmpFilePath.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return Optional.empty();
-    }
-
-
-    /*
-    * @Param mfile 報告フォームから受け取った画像ファイル
-    *
-    * 問題：写真送ったらめっちゃ時間掛かる
-    * */
-    private String base64EncodeStr(MultipartFile mfile) throws IOException {
-
-        //画像をローカルに保存
-        Optional<String> opt;
-        opt = makeTmpFile(mfile, ".jpg");
-        String path = opt.orElseGet(()->"ファイル書き込みNG");
-        System.out.println("ファイルの保存先->" + path);
-
-        //ローカルに保存した画像のBase64変換
-        File f = new File(path);
-        FileInputStream fis = new FileInputStream(f);
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        long fileSize = f.length();
-        boolean readable = false;
-
-        do {
-            byte[] bytes = new byte[1];
-            fis.read(bytes);
-
-            baos.write(bytes,0,bytes.length);
-
-            long readSize = fis.getChannel().position();
-            readable = readSize != fileSize;
-        } while(readable);
-
-        return Base64.getEncoder().encodeToString(baos.toByteArray());
-    }
-
-    /*
-    *   ちょっと変えたやつ。Ver2.0
-    *   少し早くなった、でもまだ遅い
-    * */
-    private String base64EncodeStrV2(MultipartFile mfile) throws IOException {
-
-        //画像をローカルに保存
-        Optional<String> opt;
-        opt = makeTmpFile(mfile, ".png");
-        String path = opt.orElseGet(()->"ファイル書き込みNG");
-        System.out.println("ファイルの保存先->" + path);
-
-        // ファイルインスタンスを取得し、ImageIOへ読み込ませる
-        File f = new File(path);
-        BufferedImage image = ImageIO.read(f);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        BufferedOutputStream bos = new BufferedOutputStream(baos);
-        image.flush();
-
-        // リサイズしちゃって
-        // 読み終わった画像をバイト出力へ
-        ImageIO.write(resizeImageV2(image), "png", bos);
-        bos.flush();
-        bos.close();
-
-        // バイト配列→BASE64へ変換する
-        String base64Image = Base64.getEncoder().encodeToString(baos.toByteArray());
-
-        return base64Image;
-    }
-
-    /*
-    * 画像のリサイズ(しっぱい)
-    *
-    * */
-    private BufferedImage resizeImage(BufferedImage image) {
-
-        //初期の値保存
-        int targetWidth = image.getWidth();
-        int targetHeight = image.getHeight();
-
-        //リサイズ
-        float width = 350;
-        float height = targetHeight * (width / targetWidth);
-
-        //Affine
-        AffineTransformOp affineTransformOp = new AffineTransformOp(
-                AffineTransform.getScaleInstance((double)width/width, (double)height/height ),
-                AffineTransformOp.TYPE_BILINEAR);
-        BufferedImage dstImage = new BufferedImage( (int)width, (int)height, image.getType() );
-        affineTransformOp.filter( image, dstImage);
-
-        return dstImage;
-    }
-
-    /*
-    *   成功したやつ
-    * */
-    private BufferedImage resizeImageV2(BufferedImage image) {
-
-        //初期の値保存
-        int targetWidth = image.getWidth();
-        int targetHeight = image.getHeight();
-
-        //リサイズ
-        float width = 400;
-        float height = targetHeight * (width / targetWidth);
-
-        BufferedImage resizedImg = new BufferedImage( (int)width, (int)height, BufferedImage.TRANSLUCENT);
-        Graphics2D g2 = resizedImg.createGraphics();
-        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g2.drawImage(image, 0, 0, (int)width, (int)height, null);
-        g2.dispose();
-
-        return resizedImg;
-    }
-
-    /**/
+    /*test*/
     @RequestMapping("/android_test")
     public String testpage() {
         return "android_test";
