d9a0edafc299b9343878ecdf6bdc7ec461bf9f63
S-YamaguchiC <cist.b216.s.yamaguchi@gmail.com>
2018-11-23 19:40:31 +0900

    いろいろ追加

--- src/main/java/com/example/linebot/dao/ReportDao.java
+++ src/main/java/com/example/linebot/dao/ReportDao.java
@@ -1,9 +1,42 @@
 package com.example.linebot.dao;
 
+import com.example.linebot.web.ReportBean;
+import com.google.inject.ImplementedBy;
+
 import java.sql.SQLException;
 
+/*
+ * 現行システムの " IContributionDAO.java" にあたるクラス
+ * */
+@ImplementedBy(ReportDaoImpl.class)
 public interface ReportDao {
 
+    // テスト用
     //登録(画像なし)
     void insert(int type, int category, String detail, String latitude, String longitude) throws SQLException;
+
+    /**
+     * 引数をcontributionテーブルにinsertする
+     */
+    long insertContribution(ReportBean report);
+
+    /**
+    * 引数をもとにline_idが存在するか確認する
+    * */
+    boolean existLineAccount(String line_id);
+
+    /**
+     * 引数をもとにline_idを追加する
+     * */
+    boolean registerLine(int Id, String line_id);
+
+    /**
+     * 引数をもとにLINEユーザのaccount_idを取得
+     * */
+    boolean insertAccountId(int Id, int role);
+
+    /**
+     * 存在するaccount_idの最大値を返す
+     * */
+    int getMaxId();
 }
